87a6421ff2a4de05b19a8db718e92672705550a8
Zoe Ames <zoe.ames@ingramcontent.com>
2016-10-10 13:19:59 -0500

    callback of DOOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,43 +2,44 @@
 
 $(document).ready(function() {
   console.log("jquery is ready");
+  var contentEl = $("#all-my-songs");
+  var songs = [];
   $.ajax({
     url: "../songs.json"
   }).done(function(data) {
+    songs = data.songs
     console.log("data", data);
-    // Get a reference to the DOM element that
-    // holds all the songs
-    var contentEl = $("#all-my-songs");
-    
-    /*
-      Loop through all the songs and build the
-      DOM elements for each one
-     */
-    var songData = "";
-    var currentSong;
+    $.ajax({
+      url: "../songs2.json"
+      }).done(function(data2) {
+        console.log("data2", data2);
+        var songData = "";
+        var currentSong;
+        
+        data2.songs.forEach(function(song){
+          songs.push(song);
+        })
+        console.log("songs", songs);
+        for (var i = 0; i < songs.length; i++) {
+          currentSong = songs[i];
 
-    for (var i = 0; i < data.songs.length; i++) {
-      currentSong = data.songs[i];
+          songData += "<div class='song-block'>";
+            songData += "<h1>" + currentSong.title + "</h1>";
+            songData += "<div class='artist'>Performed by ";
+              songData += currentSong.artist;
+            songData += "</div>";
+            songData += "<div class='album'>On the album ";
+              songData += currentSong.album;
+            songData += "</div>";
+          songData += "</div>";
+        }
 
-      /*
-        Build up some HTML to display the songs,
-        artists, and albums
-       */
-      songData += "<div class='song-block'>";
-        songData += "<h1>" + currentSong.title + "</h1>";
-        songData += "<div class='artist'>Performed by ";
-          songData += currentSong.artist;
-        songData += "</div>";
-        songData += "<div class='album'>On the album ";
-          songData += currentSong.album;
-        songData += "</div>";
-      songData += "</div>";
-    }
-
-    console.log("songData", songData);
-    contentEl.html(songData);
+        console.log("songData", songData);
+        contentEl.html(songData);
+      }).fail(function(error) {
+        console.log( "error" , error);
+      })
   }).fail(function(error) {
     console.log( "error" , error);
   });
-
 });
